15eeab06003a7391f3e8a376069bfde152fc840e
Jewels <jewels286@proton.me>
2025-02-27 17:27:28 -0500

    quick particle class

---
+++ calcite/core/particle.py
@@ -0,0 +1,29 @@
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class Particle:
+    mass: float
+    "The mass of the particle in GeV/c^2."
+    
+    charge: float
+    "The charge of the particle in Coulombs."
+
+    momentum: np.ndarray
+    "The momentum of the particle in GeV/c."
+
+    energy: float
+    "The energy of the particle in GeV."
+
+    velocity: np.ndarray
+    "The velocity of the particle in m/s."
+
+    position: np.ndarray
+    "The position of the particle in fm."
+
+    positions: np.ndarray
+    "The positions of the particle in fm."
+    velocities: np.ndarray
+    "The velocities of the particle in m/s."
+    momenta: np.ndarray
+    "The momenta of the particle in GeV/c."
